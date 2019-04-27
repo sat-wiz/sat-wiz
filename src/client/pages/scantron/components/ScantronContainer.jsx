e302b72037bdbf2befeab5cd30bf361510e57093
Alex Park <apark0720@gmail.com>
2019-04-27 08:04:08 -0700

    add conditional rendering to scantron container

--- src/client/pages/scantron/components/ScantronContainer.jsx
+++ src/client/pages/scantron/components/ScantronContainer.jsx
@@ -4,24 +4,41 @@ import PropTypes from 'prop-types';
 import AnswerOptions from './AnswerOptions'
 import { sectionOneKeysSelector, sectionTwoKeysSelector, sectionThreeKeysSelector, sectionFourKeysSelector } from '../selectors';
 
-function ScantronContainer({ sectionOne, currentSection }) {
-  let sectionOneElements = sectionOne.map((key) => <AnswerOptions key={`${key}`} sectionNum={key[0]} questionNum={key[1]} />);
+function ScantronContainer({ currentSection, sectionOne, sectionTwo, sectionThree, sectionFour }) {
+  const sectionOneList = sectionOne.map((key) => <AnswerOptions key={`${key}`} sectionNum={key[0]} questionNum={key[1]} />);
+  const sectionTwoList = sectionTwo.map((key) => <AnswerOptions key={`${key}`} sectionNum={key[0]} questionNum={key[1]} />);
+  const sectionThreeList = sectionThree.map((key) => <AnswerOptions key={`${key}`} sectionNum={key[0]} questionNum={key[1]} />);
+  const sectionFourList = sectionFour.map((key) => <AnswerOptions key={`${key}`} sectionNum={key[0]} questionNum={key[1]} />);
+  
+  const SECTION_LISTS = {
+    1: sectionOneList,
+    2: sectionTwoList,
+    3: sectionThreeList,
+    4: sectionFourList
+  }
+
   return (
     <>
       <h1>Scantron Container</h1>
       <p>Current section: {currentSection} </p>
-      {sectionOneElements}
+      {SECTION_LISTS[currentSection]}
     </>
   );
 }
 
 const mapStateToProps = store => ({
   sectionOne: sectionOneKeysSelector(store),
+  sectionTwo: sectionTwoKeysSelector(store),
+  sectionThree: sectionThreeKeysSelector(store),
+  sectionFour: sectionFourKeysSelector(store),
   currentSection: store.scantron.currentSection,
 });
 
 ScantronContainer.propTypes = {
   sectionOne: PropTypes.array.isRequired,
+  sectionTwo: PropTypes.array.isRequired,
+  sectionThree: PropTypes.array.isRequired,
+  sectionFour: PropTypes.array.isRequired,
   currentSection: PropTypes.number.isRequired,
 }
 
