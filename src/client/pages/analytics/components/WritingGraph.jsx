a82cb9836ac6423bf27c8bedf1630e1d621786d8
Johnny Lim <bluejayl@ucla.edu>
2019-04-29 19:47:37 -0700

    project is missing connection with front and back end

--- src/client/pages/analytics/components/WritingGraph.jsx
+++ src/client/pages/analytics/components/WritingGraph.jsx
@@ -11,19 +11,19 @@ import {
 } from 'react-vis';
 
 const DATA = [
-  {x: 1, y: 4, size: 9},
-  {x: 1, y: 5, size: 18},
-  {x: 1, y: 10, size: 5},
-  {x: 1, y: 11, size: 29},
-  {x: 1, y: 13.9, size: 5},
-  {x: 1, y: 14, size: 8},
+  {x: 1, y: 4, size: 12},
+  {x: 1, y: 5, size: 20},
+  {x: 1, y: 10, size: 25},
+  {x: 1, y: 11, size: 22},
+  {x: 1, y: 13.9, size: 3},
+  {x: 1, y: 14, size: 18},
   {x: 1.5, y: 11.8, size: 25},
   {x: 1.7, y: 9, size: 30},
   {x: 2, y: 5, size: 11},
   {x: 2.1, y: 11.8, size: 28},
-  {x: 2.4, y: 7.9, size: 14},
+  {x: 2.4, y: 7.9, size: 30},
   {x: 2.4, y: 13.5, size: 20},
-  {x: 2.7, y: 13.7, size: 14},
+  {x: 2.7, y: 13.7, size: 23},
   {x: 2.9, y: 7.7, size: 26},
   {x: 3, y: 5.4, size: 6}
 ].map((d, id) => ({...d, id}));
@@ -31,8 +31,8 @@ const DATA = [
 
 // magic numbers chosen for design
 const sizeRange = [5, 13];
-const width = 300;
-const height = 300;
+const width = 800;
+const height = 800;
 
 class WritingGraph extends React.Component {
     state = {
